216fd3d1580f7303535933a3ca7804e66a508b6c
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-07-15 05:51:18 +0530

    Day 6: Practiced JavaScript Strings

--- 0_Template/exercises.js
+++
@@ -1,9 +0,0 @@
-const name = "Rahul";
-const age = 25;
-const city = "Bihar";
-const profession = "Engineer";
-
-console.log(name, age, city, profession);
-
-
-// Logic: Date Store -> Process -> Display -> Success. 
--- 0_Template/mini-project.js
+++
@@ -1,9 +0,0 @@
-const num1 = 30;
-const num2 = 10;
-
-const sum = num1 + num2;
-const subtraction = num1 - num2;
-
-console.log("The sum of", num1, "and", num2, "is:", sum);
-
-console.log("The subtraction of", num1, "and", num2, "is:", subtraction);
