23a741af525ac3518d412b77e3e3c8ad35260631
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-07-20 16:54:03 +0530

    Day 9: (Evening): Practiced Boolean, Null, Undefined, BigInt, Symbol, and typeof.
    Day 9: (Evening): Practiced Boolean, Null, Undefined, BigInt, Symbol, and typeof.

--- 03_Data Types/5_Practice/practice.js
+++ 03_Data Types/5_Practice/practice.js
@@ -12,10 +12,73 @@
 
 // 2.Stores numeric values including age, servicesPrice and monthlyRevenue.
 
-const age = 25;
-const servicePrice = 12000;
-const monthlyRevenue = 300000;
+// const age = 25;
+// const servicePrice = 12000;
+// const monthlyRevenue = 300000;
 
-console.log("Age: " + age);
-console.log("Services Price: " + servicePrice);
-console.log("Monthly Revenue: " + monthlyRevenue);
+// console.log("Age: " + age);
+// console.log("Services Price: " + servicePrice);
+// console.log("Monthly Revenue: " + monthlyRevenue);
+
+// 3. Store logical values.
+
+// const isAdmin = false;
+// const paymentCompleted = true;
+// const isLoggedIn = false;
+
+// console.log(isAdmin);
+// console.log(paymentCompleted);
+// console.log(isLoggedIn);
+
+// 4.Store an intentional empty value.
+
+// const profilePhoto = null;
+// const manager = null;
+// const assignedProject = null;
+
+// console.log(profilePhoto);
+// console.log(manager);
+// console.log(assignedProject);
+
+// 5.Create variables without assigning values.
+
+// let clientId;
+// let phoneNumber;
+// let address;
+
+// console.log(clientId);
+// console.log(phoneNumber);
+// console.log(address);
+
+// 6.Create unique identifiers.
+
+// const userId = Symbol("user");
+// const productId = Symbol("product");
+
+// console.log(userId);
+// console.log(productId);
+
+// 7. Store very large integers.
+
+// const population = 13438413843n;
+// const bankBalance = 1000000000n;
+// console.log(population);
+// console.log(bankBalance);
+
+// 7. Check the Data Type Using typeof
+
+const name = "Rahul";
+const price = 25000;
+const isAdmin = true;
+const profilePhoto = "null";
+let assignedProject;
+const userId = Symbol("user");
+const bankBalance = 1000000n;
+
+console.log(typeof name);
+console.log(typeof price);
+console.log(typeof isAdmin);
+console.log(typeof profilePhoto);
+console.log(typeof assignedProject);
+console.log(typeof userId);
+console.log(bankBalance);
