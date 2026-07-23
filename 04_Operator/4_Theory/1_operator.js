5c2179dd3696c24ef55f03f348694a5b2f57a4ee
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-07-23 06:14:11 +0530

    Day 12 (Morning): Understand Operators, Arithmetic.
    Day 12 (Morning): Understand Operators, Arithmetic.

---
+++ 04_Operator/4_Theory/1_operator.js
@@ -0,0 +1,4 @@
+const price = 50000;
+const quantity = 2;
+const total = price * quantity;
+console.log(`Total ${total}`);
---
+++ 04_Operator/4_Theory/2_arthmetic.js
@@ -0,0 +1,31 @@
+// const value1 = 25;
+// const value2 = 50;
+// const total = value1 + value2;
+// console.log(total);
+
+// Business Example: Revenue Calculation
+
+// const offlineSales = 200000;
+// const onlineSales = 300000;
+// const totalRevenue = offlineSales + onlineSales;
+// console.log(`Total Revenue ${totalRevenue}`);
+
+// Subtraction Operator : Profit calculation
+
+// const sellingPrice = 1200;
+// const costPrice = 900;
+// const profit = sellingPrice - costPrice;
+// console.log(`Total Profit ${profit}`);
+
+// Multiplication Operators
+// const productPrice = 2500;
+// const quantity = 5;
+// const totalPrice = productPrice * quantity; 
+// console.log(totalPrice);
+
+// Division Operators
+
+const totalMarks = 370;
+const totalSubjects = 6;
+const average = totalMarks / totalSubjects;
+console.log(`The Marks Average is: ${average}`);
