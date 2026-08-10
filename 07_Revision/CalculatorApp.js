2d71adcffc9bdae1c001d887752e27397d19789f
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-10 10:23:04 +0530

    Day 24 (Morning) : tried to apply validation in calculator app.
    Day 24 (Morning) : tried to apply validation in calculator app.

---
+++ 07_Revision/CalculatorApp.js
@@ -0,0 +1,18 @@
+// Challenge 1
+const number1 = 25;
+const number2 = 25;
+const addition = number1 + number2;
+const subtraction = number1 - number2;
+const multiplication = number1 * number2;
+const division = number1 / number2;
+if(number1 === 0){
+    console.log("Cannot divided");
+} else{
+    console.log("Can divided by zero.");
+}
+console.log(`Additioin: ${addition}`);
+console.log(`Subtraction: ${subtraction}`);
+// console.log("Multiplication:" + multiplication);
+console.log(`Multiplication: ${multiplication}`);
+// console.log("Division: " + division);
+console.log(`Division: ${division}`);
