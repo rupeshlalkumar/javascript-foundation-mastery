3004dbef20f3848371433fd79a6ba794111a5f76
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-16 16:30:39 +0530

    Day 29 (Afternoon) : I tried to build small calculator app.
    Day 29 (Afternoon) : I tried to build small calculator app.

---
+++ 07_Test/Calculator.js
@@ -0,0 +1,14 @@
+const number1 = 20;
+const number2 = 10;
+
+const addition = number1 + number2;
+console.log(`Total Sum: ${addition}`);
+
+const subtraction = number1 - number2;
+console.log(`Total Subtraction: ${subtraction}`);
+
+const multiplication = number1 * number2;
+console.log(`Total Multipliaction: ${multiplication}`);
+
+const division = number1 / number2;
+console.log(`Total Division: ${division}`);
