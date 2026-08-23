3ea9f41a2f2984f8458a09c4f6a2be79b4ec7f82
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-23 07:42:03 +0530

    Day 31 (Morning): Theory Practice.
    Day 31 (Morning): Theory Practice.

---
+++ 08_Function/4_Theory/Logic.js
@@ -0,0 +1,30 @@
+// function greet(){
+//     console.log("Hello World!");
+// }
+
+// function greet(name) {
+//   console.log(`Hello My Dear Friend, ${name}`);
+// }
+
+// greet("Vasudev");
+// greet("Krishna");
+// greet("Balram");
+
+// function add (a,b){
+//     return a + b;
+// }
+// const result = add (10,20);
+// console.log(result);
+
+// function cal (a,b){
+//     console.log(`Total, ${a,b}`);
+// }
+// cal (30, 40);
+// cal (40,50);
+
+function add(a,b){
+    return a + b;
+}
+
+const result = add (10,30);
+console.log(result);
