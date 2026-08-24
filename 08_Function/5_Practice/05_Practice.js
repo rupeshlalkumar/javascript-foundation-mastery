1820d6beb268b81ff08f8b8a2c9f87b947158186
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-24 09:42:30 +0530

    Day 32 (Morning) : Practice Function Parameter
    Day 32 (Morning) : Practice Function Parameter

---
+++ 08_Function/5_Practice/05_Practice.js
@@ -0,0 +1,13 @@
+// 1. Without Parameter Function
+
+// function greet(){
+//     console.log(`Hello`);
+// }
+// greet();
+
+// 02. One Parameter Function
+
+function greet(name){
+    console.log(`Hello Mr. ${name}`);
+}
+greet("Rupesh");
