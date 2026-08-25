5a28cb95e0e3aeb6a8184295cad3e302454e8843
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-25 09:34:59 +0530

    Day 33 (Morning) : Practice Two and Three parameter function.
    Day 33 (Morning) : Practice Two and Three parameter function.

--- 08_Function/5_Practice/05_Practice.js
+++ 08_Function/5_Practice/05_Practice.js
@@ -7,7 +7,22 @@
 
 // 02. One Parameter Function
 
-function greet(name){
-    console.log(`Hello Mr. ${name}`);
+// function greet(name){
+//     console.log(`Hello Mr. ${name}`);
+// }
+// greet("Rupesh");
+
+// 03. Two Parameter Function
+
+// function greet(name, village){
+//     console.log(`Hello, ${name}, ${village}`);
+// }
+// greet("Rupesh", "Murarpur");
+
+// 04. Three Parameter Function
+
+function greet(name, village, dist){
+    console.log(`Hello, ${name}, ${village}, ${dist}`);
 }
-greet("Rupesh");
+
+greet("name", "Murarpur", "Bhagalpur");
