4bcf5a6e14c09db2ea2ea3b96043b961d119af61
Rupesh Lal Kumar <rupeshkumar2575135@gmail.com>
2026-08-26 06:28:33 +0530

    Day 34 (Morning) : Practice return, console.log and function execution flow.
    Day 34 (Morning) : Practice return, console.log and function execution flow.

--- 08_Function/5_Practice/05_Practice.js
+++ 08_Function/5_Practice/05_Practice.js
@@ -21,8 +21,55 @@
 
 // 04. Three Parameter Function
 
-function greet(name, village, dist){
-    console.log(`Hello, ${name}, ${village}, ${dist}`);
-}
+// function greet(name, village, dist){
+//     console.log(`Hello, ${name}, ${village}, ${dist}`);
+// }
 
-greet("name", "Murarpur", "Bhagalpur");
+// greet("name", "Murarpur", "Bhagalpur");
+
+// 5. Without Return Function 
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// const result = add(10,20);
+// console.log(result);
+
+// 6. With Return Function
+
+// function add(a,b){
+//     return a+b;
+// const result = add(10,20);
+// console.log(result);
+
+// Subtaction
+
+// function minus(a,b){
+//     return a-b;
+// }
+// const result = minus(30,20);
+// console.log(result);
+
+// Multiplication
+
+// function multiplication(a,b){
+//     return a*b;
+// }
+// const result = multiplication(10,20);
+// console.log(result);
+
+// Division
+
+// function division(a,b){
+//     return a/b;
+// }
+// const result = division(100,10);
+// console.log(result);
+
+// Profit Calculator
+
+function profit(a,b){
+    return a-b;
+}
+const totalProfit = profit(10000,6500);
+console.log(totalProfit);
