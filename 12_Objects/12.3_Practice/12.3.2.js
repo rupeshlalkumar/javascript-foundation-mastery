0eb0d8f2bd4fd01266d10f15e830bc5705800e86
rupeshlalkumar <rupeshkumar2575135@gmail.com>
2026-09-24 10:33:05 +0530

    Day 51 (Morning): Practiced Object Method, Properties.
    Day 51 (Morning): Practiced Object Method, Properties.

---
+++ 12_Objects/12.3_Practice/12.3.2.js
@@ -0,0 +1,9 @@
+const students = {
+name: "Krishna",
+age: 20,
+course: "English"
+};
+
+console.log(students.name);
+console.log(students.age);
+console.log(students.course);
---
+++ 12_Objects/12.3_Practice/13.3.3.js
@@ -0,0 +1,10 @@
+const students = {
+    name: "Krishna",
+    age: 20,
+    course: "English",
+    showInfo: function()  {
+        console.log(students);
+    }
+};
+
+students.showInfo();
