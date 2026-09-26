ca07acc083ce4775c408984cdade94cea953deda
rupeshlalkumar <rupeshkumar2575135@gmail.com>
2026-09-26 08:47:30 +0530

    Day 53 (Morning) : Practiced Nested Object.
    Day 53 (Morning) : Practiced Nested Object.

---
+++ 12_Objects/12.3_Practice/12.3.5.js
@@ -0,0 +1,11 @@
+const customer = {
+    name: "Rahul",
+    phone: "78705556678",
+    address: {
+        village: "Gauripur",
+        city: "Bhagalpur",
+        state: "Bihar",
+    },
+};
+
+console.log(customer.address.village);
